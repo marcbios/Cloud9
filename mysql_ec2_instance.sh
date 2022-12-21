0418268c8a733b113f5b24ff217eec6934d8dc6b
Marcus Olatoye <43044527+marcbios@users.noreply.github.com>
2022-12-20 18:09:10 -0800

    Add files via upload

---
+++ mysql_ec2_instance.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+db_root_password=Stackinc@11-root
+db_username=stack_wordpress_marcus
+db_user_password=Stackinc@11-user
+db_name=stack_jul22_mar_wp_db
+
+mysql -sfu root <<EOS
+-- set root password
+UPDATE mysql.user SET Password=PASSWORD('$db_root_password') WHERE User='root';
+-- delete anonymous users
+DELETE FROM mysql.user WHERE User='';
+-- delete remote root capabilities
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- drop database 'test'
+DROP DATABASE IF EXISTS test;
+-- also make sure there are lingering permissions to it
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+-- make changes immediately
+FLUSH PRIVILEGES;
+EOS
+
+if (( $? == 0 ))
+then
+        echo "success!"
+else
+        echo "fail!"
+        exit
+fi
